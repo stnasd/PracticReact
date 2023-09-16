07aeee2ad6b2fc9a529d84da1f305bef98511870
Kazimir Aleksandr <Stnasdasd@gmail.com>
2023-09-16 22:19:51 +0300

    fixed confilcts

--- src/components/pages/SignupPage/SignupPage.js
+++ src/components/pages/SignupPage/SignupPage.js
@@ -1,5 +1,4 @@
 import './SignupPage.scss'
-<<<<<<< HEAD
 import Form from '../../Form/Form';
 import { motion } from 'framer-motion'
 import { useSignupMutation } from '../../../apiFirebase/apiFireBaseSlice';
@@ -12,16 +11,6 @@ const SignupPage = () => {
         createHero({ email, pass }).unwrap()
         navigate('/login')
     }
-=======
-import { motion } from 'framer-motion'
-
-
-
-
-
-
-const SignupPage = () => {
->>>>>>> 8af18e691da3026bd24648674fadd759ad96fd91
 
     return (
         <motion.div
@@ -30,33 +19,30 @@ const SignupPage = () => {
             animate={{ opacity: 1, transition: { duration: 0.3 } }}
             exit={{ opacity: 0, transition: { duration: 0.1 } }}
         >
-<<<<<<< HEAD
             <Form title="Зарегистрироваться" onHandleSubmit={onHandleSubmit} />
-=======
             <div className='signup__text'>Регистрация</div>
             <div className='signup__block'>
                 <input
                     className='signup__block-input'
                     type="email"
-                    // value={email}
-                    // onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     placeholder="email"
                 />
                 <input
                     className='signup__block-input'
                     type="password"
-                    // value={pass}
-                    // onChange={(e) => setPass(e.target.value)}
+                    value={pass}
+                    onChange={(e) => setPass(e.target.value)}
                     placeholder="password"
                 />
                 <button
                     className='signup__block-button'
-                // onClick={() => handleClick(email, pass)}
+                    onClick={() => handleClick(email, pass)}
                 >
                     Зарегистрироваться
                 </button>
             </div>
->>>>>>> 8af18e691da3026bd24648674fadd759ad96fd91
         </motion.div>
     );
 };
