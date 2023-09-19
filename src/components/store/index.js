ed79403c5ea03398c7258f33ae50afed16eae1c5
Kazimir Aleksandr <Stnasdasd@gmail.com>
2023-09-19 22:11:03 +0300

    delete formSlice

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import characters from '../pages/MainPage/MainPageSlice'
 import login from '../pages/LoginPage/LoginPageSlice'
-import form from '../Form/FormSlice'
 import { apiFireBaseSlice } from '../../apiFirebase/apiFireBaseSlice';
 
 const store = configureStore({
-    reducer: { characters, login, form, [apiFireBaseSlice.reducerPath]: apiFireBaseSlice.reducer },
+    reducer: { characters, login, [apiFireBaseSlice.reducerPath]: apiFireBaseSlice.reducer },
     middleware: getDefaultMidleWare => getDefaultMidleWare().concat(apiFireBaseSlice.middleware),
     devTools: process.env.NODE_ENV !== 'production'
 })
