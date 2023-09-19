3492565fe37856e95cc0b17f2991517a823bf3bd
Kazimir Aleksandr <Stnasdasd@gmail.com>
2023-09-19 23:58:25 +0300

    fixed confilcts

--- src/components/CharsList/CharsList.js
+++ src/components/CharsList/CharsList.js
@@ -4,21 +4,22 @@ import { Link } from 'react-router-dom'
 const CharsList = ({ charactersList, userAuthorized }) => {
 
     const renderItems = (arr, userLogin) => {
-        const renderButtons = (userLogin) => {
-            if (userLogin === true) {
-                return (
-                    <>
-                        <button className='button__char-add'>add</button>
-                        <button className='button__char-delete'>x</button>
-                    </>
-                )
-            } else {
-                return null
-            }
-        }
 
-        const buttons = renderButtons(userLogin)
         return arr.map(item => {
+            const renderButtons = (userLogin) => {
+                if (userLogin === true) {
+                    return (
+                        <>
+                            <button className='button__char-add'>add</button>
+                            <button className='button__char-delete'>x</button>
+                        </>
+                    )
+                } else {
+                    return null
+                }
+            }
+
+            const buttons = renderButtons(userLogin)
             const { image, name, id, origin } = item
             return (
                 <div
@@ -30,7 +31,7 @@ const CharsList = ({ charactersList, userAuthorized }) => {
                     <div className="char__name">name : {name}</div>
                     <div className="char__item-playedby">Origin : {origin}</div>
                     {buttons}
-                    <Link to="/info"><button className='char__info-button'>Больше информации</button></Link>
+                    <Link to="/info"><button>Больше информации</button></Link>
                 </div>
             )
         })
--- src/components/pages/LoginPage/LoginPage.js
+++ src/components/pages/LoginPage/LoginPage.js
@@ -14,7 +14,6 @@ const LoginPage = () => {
 
     const onHandleSubmit = (args) => {
         const data = trigger(args)
-        console.log(data)
         data.then((res) => {
             if (res.data === 'ok') {
                 dispatch(userLogin())
