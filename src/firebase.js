ff7241086f2dc2d127ba472928206e520e715c66
Kazimir Aleksandr <Stnasdasd@gmail.com>
2023-09-27 16:48:31 +0300

    env

--- src/firebase.js
+++ src/firebase.js
@@ -3,17 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDiNzVW48s5d4rfohT_pgHLwg150lhgq5w",
-    authDomain: "reactpractic2.firebaseapp.com",
-    projectId: "reactpractic2",
-    storageBucket: "reactpractic2.appspot.com",
-    messagingSenderId: "808196092824",
-    appId: "1:808196092824:web:b163d1585c796e19ed71ed",
-    databaseURL: "https://reactpractic2-default-rtdb.firebaseio.com"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_ROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BACKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 export const appFirebase = initializeApp(firebaseConfig);
 export const db = getFirestore(appFirebase);
 export const auth = getAuth(appFirebase);
-
-
